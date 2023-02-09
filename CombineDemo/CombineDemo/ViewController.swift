1f41f7b2a79fc5a3f8459addd7f3b52bebc8e344
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-02-09 10:30:03 +0530

    Subscriber Subject

--- CombineDemo/CombineDemo/ViewController.swift
+++ CombineDemo/CombineDemo/ViewController.swift
@@ -6,16 +6,95 @@
 //
 
 import UIKit
+import Combine
+
+enum SubscriberError: Error {
+    case subscriberError
+    
+}
+
+
+
+class stringSubscriber: Subscriber {
+   
+    typealias Input = String
+    
+    typealias Failure = SubscriberError
+    
+  
+    
+    func receive(subscription: Subscription) {
+        subscription.request(.max(2))
+        
+    }
+    
+    func receive(_ input: String) -> Subscribers.Demand {
+        print("Received Value: \(input)")
+        return .none
+    }
+    
+    func receive(completion: Subscribers.Completion<SubscriberError>) {
+        print("Completed")
+    }
+    
+    
+    
+    
+}
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getNotificationTrigger()
+       // getNotificationTrigger()
+        
+        
+        let publisher = ["a","b","c","d", "e", "f"].publisher
+      //  publisher.sink { value in
+      //      print(value)
+      //  }
+        
+        
+        
+        
+        let subscriber = stringSubscriber()
+        
+        let subject = PassthroughSubject<String, SubscriberError>()
+        
+        subject.subscribe(subscriber)
+       // publisher.subscribe(subscriber)
+        
+        let subscription = subject.sink { completion in
+            print("completion Value from sink")
+        } receiveValue: { value in
+            print("Recieved Sink")
+        }
+
+        
+        subject.send("A")
+        subject.send("B")
+        
+        
+        subscription.cancel()
+        subject.send("C")
+        
+        
+        
+        
+        
+        
+       
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
     
     func getNotificationTrigger() {
         
